305f323ad1b656ecf2487884ba7d81c3ba885842
jgdmeyer <demeyer.jeroen@hotmail.com>
2016-09-07 00:43:02 +0200

    Invalid testcases added

--- Tester.h
+++ Tester.h
@@ -1,1 +1,29 @@
-#pragma once
+#ifndef TESTER_H
+#define TESTER_H
+
+#define EMTY_Q ""
+//invalid char, only 1,0,- allowed in input string
+#define INVALID_Q1 "101010010101011010010101101001a1011010"
+//invalid dimensions, 35 char
+#define INVALID_Q2 "1010100101010110100101011010011011010"
+//invalid dimensions, 37 char
+#define INVALID_Q3 "101010010101011010010101101001010110101"
+
+#define Q1 "123456789123456789123456789123456789"
+#define Q2 ""
+#define Q3 ""
+#define Q4 ""
+#define Q5 ""
+#define Q6 ""
+#define Q7 ""
+
+#define A1 ""
+#define A2 ""
+#define A3 ""
+#define A4 ""
+#define A5 ""
+#define A6 ""
+#define A7 ""
+
+#endif // !TESTER_H
+
