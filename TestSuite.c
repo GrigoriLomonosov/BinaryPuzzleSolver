ecce41dd1f717b645d77e01b2cb8abd6c53d6d3c
jgdmeyer <demeyer.jeroen@hotmail.com>
2016-09-08 01:17:10 +0200

    functionheaders made for testing

--- TestSuite.c
+++ TestSuite.c
@@ -1,18 +1,13 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "Tester.h"
 
-#include "BinaryPuzzle.h"
-
-int compareStrings(char* first, char* second) {
-
+int main() {
+	printf("Testing BinaryPuzzleSolver");
+	test_init_puzzle("");
+	test_find_pairs();
+	test_avoid_trios();
+	test_complete_RC();
+	test_complete_half_RC();
+	test_eliminate_impossible_combos();
+	test_eliminate_other_impossible_combos();
+	test_general_solver();
 }
-
-int compareArrays(int* first, int firstLength, int* second, int secondLength) {
-
-}
-
-int checkPuzzle(BinaryPuzzle puzzle) {
-
-}
-
-int main()
--- Tester.c
+++ Tester.c
@@ -1,1 +1,33 @@
-#pragma once
+#include "Tester.h"
+
+int test_init_puzzle(char* input) {
+	return 0;
+}
+
+int test_find_pairs() {
+	return 0;
+}
+
+int test_avoid_trios() {
+	return 0;
+}
+
+int test_complete_RC() {
+	return 0;
+}
+
+int test_eliminate_impossible_combos() {
+	return 0;
+}
+
+int test_complete_half_RC() {
+	return 0;
+}
+
+int test_eliminate_other_impossible_combos() {
+	return 0;
+}
+
+int test_general_solver() {
+	return 0;
+}
